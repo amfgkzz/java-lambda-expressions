b216ce651b966d7fbacea4be1b515bdf8893e72f
Koua Xiong <kxiong517@gmail.com>
2019-08-21 11:07:55 -0500

    made some notes to myself

--- transportation/Main.java
+++ transportation/Main.java
@@ -4,11 +4,31 @@ import java.util.*;
 
 public class Main
 {
+    // typing -> pvm -> results in 
+    // private void name() {
+        
+    // }
+
+    // typing -> pvstm -> results in
+    // private static Type name() {
+        
+    // }
+
+    // typing -> pbcm -> results in
+    // public void name() {
+        
+    // }
+
+    // typing -> pbcsm -> results in
+    // public static void name() {
+        
+    // }
+
     public static void main(String[] args)
     {
         Horse seabiscuit = new Horse("Kong");
         Horse bartholomus = new Horse("Tim");
-        Horse bitch = new Horse("Kevin");
+        Horse doug = new Horse("Kevin");
 
         seabiscuit.eat(10);
         bartholomus.eat(25);
@@ -16,5 +36,6 @@ public class Main
         seabiscuit.move();
 
         System.out.println("seabiscuit's fuel level " + seabiscuit.getFuelLevel());
+        System.out.println("Kevin talk to me " + doug.speak());
     }
 }
